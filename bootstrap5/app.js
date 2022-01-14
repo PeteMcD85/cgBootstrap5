10437a4d2094d97edaede567f367609631974b70
Peter McDonald <petermcdonald@Peters-MacBook-Air.local>
2022-01-14 12:45:20 -0500

    first coloumn accordians to certain section are done

--- bootstrap5/app.js
+++ bootstrap5/app.js
@@ -1,17 +1,25 @@
-$(function() {
-  let w = window.innerWidth,
-    viewport = document.querySelector('meta[name="viewport"]');
-
-  console.log(w);
+$(document).ready(function() {
+  let viewport = $('meta[name="viewport"]'),
+    windowWidth = $(window).width();
+  console.log("viewport");
   console.log(viewport);
 
-  document.getElementById("desktopVersion").addEventListener("click", resize);
-  document.getElementById("mobileVersion").addEventListener("click", resizeMobile);
+  $("#mobileVersion").click(resizeMobile);
+  // document.getElementById("mobileVersion").addEventListener("click", resizeMobile);
+  $("#desktopVersion").click(resize);
+  // document.getElementById("desktopVersion").addEventListener("click", resize);
+
+  $(window).resize(function() {
+    windowWidth = $(window).width();
+    console.log("windowWidth");
+    console.log(windowWidth);
+  });
+
   //Resize window
   function resize() {
-    if (w <= 768) {
-      viewport.setAttribute("content", content = "width = 1060px, maximum - scale = 1.0, User-scalable=yes");
-      $('.desktop').removeClass('mx-md-1').addClass('mx-1');
+    if (windowWidth <= 576) {
+      // viewport.setAttribute("content", content = "width = 1060px, maximum - scale = 1.0, User-scalable=yes");
+      viewport.attr("content", "width = 1060px, maximum - scale = 1.0, User-scalable=yes")
       $('.desktop-row').removeClass('row').addClass('column');
     }
     // end of ifStatement
@@ -19,10 +27,9 @@ $(function() {
   // end of resize()
 
   function resizeMobile() {
-    viewport.setAttribute("content", content = "width=device-width, initial-scale=1");
-    $('.desktop').removeClass(' mx-1').addClass('mx-md-1');
+    viewport.attr("content", content = "width=device-width, initial-scale=1");
     $('.desktop-row').removeClass('column').addClass('row');
   }
   // end of resizeMobile()
 });
-// end of app.js function
+// end of doc ready function
