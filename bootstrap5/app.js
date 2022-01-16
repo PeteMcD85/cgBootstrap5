e73ca97f6e4b14718a262fe2ee877d43edb5f546
Peter McDonald <petermcdonald@Peters-MacBook-Air.local>
2022-01-16 12:00:08 -0500

    mobile looking perfect, have to talk to stephen to make sure.. going to start on desktop

--- bootstrap5/app.js
+++ bootstrap5/app.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
-  let viewport = $('meta[name="viewport"]'),
-    windowWidth = $(window).width();
+  var viewport = $('meta[name="viewport"]'),
+    windowWidth = $(window).width(),
+    currentHTMLinsertAfter = $("#get-search-advance-potd-content").insertAfter($("#desktop-search-advance-potd")),
+    currentHTMLcontents = $("#get-search-advance-potd-content").contents($("#search-advance-potd")),
+    htmlFunction = $("#search-advance-potd").html(),
+    innerHtmlFunction = $("#search-advance-potd").innerHTML,
+    textFunction = $("#search-advance-potd").text();
+     // var one = $('.a').innerHTML
+
   console.log("viewport");
   console.log(viewport);
+  console.log("currentHTMLinsertAfter");
+  console.log(currentHTMLinsertAfter);
+  console.log("currentHTMLcontents");
+  console.log(currentHTMLcontents);
+  console.log('htmlFunction');
+  console.log(htmlFunction);
+  console.log('innerHtmlFunction');
+  console.log(innerHtmlFunction);
+  console.log('textFunction');
+  console.log(textFunction);
 
   $("#mobileVersion").click(resizeMobile);
   // document.getElementById("mobileVersion").addEventListener("click", resizeMobile);
@@ -14,14 +31,25 @@ $(document).ready(function() {
     console.log("windowWidth");
     console.log(windowWidth);
     if (windowWidth >= 576) {
-      console.log("greater thatn 576");
+      console.log("greater than 576");
       $(".middle-bottom-accordion").removeClass('border').addClass('border-none');
       $(".middle-bottom-accordion").removeClass('border-1');
       $(".middle-bottom-accordion").removeClass('border-dark');
+      // var currentHTML = $("#search-advance-potd").html();
+      // console.log('currentHTML');
+      // console.log(currentHTML);
+      $("#desktop-search-advance-potd").html(currentHTMLinsertAfter);
+      // $("#search-advance-potd").html(currentHTML);
+      // $(".columns-one-order").removeClass('order-2').addClass('order-1');
+      // $(".columns-two-order").removeClass('order-1').addClass('order-2');
+      // $(".columns-three-order").removeClass('order-3').addClass('order-3');
     } else {
       $(".middle-bottom-accordion").addClass('border');
       $(".middle-bottom-accordion").addClass('border-1');
       $(".middle-bottom-accordion").addClass('border-dark');
+      // $(".columns-one-order").removeClass('order-1').addClass('order-2');
+      // $(".columns-two-order").removeClass('order-2').addClass('order-1');
+      // $(".columns-three-order").removeClass('order-3').addClass('order-3');
     }
   });
 
@@ -31,6 +59,7 @@ $(document).ready(function() {
       // viewport.setAttribute("content", content = "width = 1060px, maximum - scale = 1.0, User-scalable=yes");
       viewport.attr("content", "width = 1060px, maximum - scale = 1.0, User-scalable=yes")
       $('.desktop-row').removeClass('row').addClass('column');
+
     }
     // end of ifStatement
   }
